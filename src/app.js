fab8ae9b4632dd8133393991890f7a6c95014a8a
Priyanshurajputpr <rajputpriyansh2001@gmail.com>
2026-03-26 18:47:08 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 //This is the strarting file of your application .... this the main core js file where we will write nodeJs code
 
-const express  = require("express");
+const express = require("express");
 const app = express();
 
-app.get("/user/:userID/:name/:password",(req,res)=>{
-  console.log(req.params);
-  res.send("Getting the respone");
-});
- 
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Response 1");
+   next();
+    // res.send("Route Handler 1");
+  },
+  (req, res,next) => {
+    console.log("Response 2");
+    // res.send("Route Handler 2");
+    next();
+  },
+    (req, res,next) => {
+    console.log("Response 3");
+    // res.send("Route Handler 3");
+    next();
+  },
+    (req, res) => {
+    console.log("Response 4");
+    res.send("Route Handler 4");
+  },
+);
 
-app.listen(3000 , ()=>{
+app.listen(3000, () => {
   console.log("Server is responding on port 3000 ....");
-}); 
+});
