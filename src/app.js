ae245236c59ef9750471ba77545f0e6f5454e56f
Priyanshurajputpr <rajputpriyansh2001@gmail.com>
2026-03-27 10:14:43 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -3,25 +3,55 @@
 const express = require("express");
 const app = express();
 
-const {authAdmin, authUser} = require("./middlewares/auth");
+// const {authAdmin, authUser} = require("./middlewares/auth");
 
-app.use("/admin",authAdmin);
+// app.use("/admin",authAdmin);
 
-app.use("/user/login",(req,res)=>{
-  res.send("Logged in successfully!!");
-})
+// app.use("/user/login",(req,res)=>{
+//   res.send("Logged in successfully!!");
+// })
 
-app.use("/user/data",authUser,(req,res)=>{
-  res.send("User data sent");
+// app.use("/user/data",authUser,(req,res)=>{
+//   res.send("User data sent");
+// });
+
+// app.get("/admin/getAllData",(req,res)=>{
+//   res.send("Got All Data");
+// });
+// app.get("/admin/deleteUser",(req,res)=>{
+//   res.send("Deleted the user");
+// })
+
+
+// app.use("/error",(err,req,res,next)=>{
+//   if(err){
+//     res.status(501).send("Something went wrong");
+//   }
+// });
+
+app.use("/",(err,req,res,next)=>{
+  if(err){
+    res.status(500).send("Something went wrong");
+  }
 });
 
-app.get("/admin/getAllData",(req,res)=>{
-  res.send("Got All Data");
+app.use("/getUserData",(req,res)=>{
+    try{
+    throw new Error("iuuivr");
+    res.send("User Data Sent");
+    }
+    catch(err){
+      res.status(500).send("Contact support");
+    }
 });
-app.get("/admin/deleteUser",(req,res)=>{
-  res.send("Deleted the user");
-})
+
+app.use("/",(err,req,res,next)=>{
+  if(err){
+    res.status(500).send("Something went wrong");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is responding on port 3000 ....");
 });
+ 
